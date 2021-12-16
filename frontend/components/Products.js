3b0f9878d661f71daf63ada0982b51fc897139c4
Lan Phan <lanc_phan@hotmail.com>
2021-12-16 00:14:13 -0500

    created the products component to render the products from the backend

---
+++ frontend/components/Products.js
@@ -0,0 +1,35 @@
+import { useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
+import Product from './Product';
+
+const ALL_PRODUCTS_QUERY = gql`
+  query ALL_PRODUCTS_QUERY {
+    allProducts {
+      id
+      name
+      price
+      description
+      photo {
+        id
+        image {
+          publicUrlTransformed
+        }
+      }
+    }
+  }
+`;
+
+export default function Products() {
+  const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+  return (
+    <div>
+      <div>
+        {data.allProducts.map((product) => (
+          <Product key={product.id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
+}
