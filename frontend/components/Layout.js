23fd564e626596edf2d81ab260702a9eaa3be463
Lan Phan <lanc_phan@hotmail.com>
2021-12-15 21:18:48 -0500

    created the header and nav components

--- frontend/components/Layout.js
+++ frontend/components/Layout.js
@@ -1,8 +1,10 @@
 import PropTypes from 'prop-types';
+import Header from './Header';
 
 export default function Layout({ children }) {
   return (
     <div>
+      <Header />
       <h2>I am a layout component</h2>
       {children}
     </div>
--- frontend/pages/sell.js
+++ frontend/pages/sell.js
@@ -1,9 +1,7 @@
-import Layout from '../components/Layout';
-
 export default function SellPage() {
   return (
-    <Layout>
+    <div>
       <p>sell page!</p>
-    </Layout>
+    </div>
   );
 }
