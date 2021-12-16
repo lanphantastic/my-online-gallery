4a6aeafe11fd9dcbbf6523b0e937cd06ac733aea
Lan Phan <lanc_phan@hotmail.com>
2021-12-15 22:49:25 -0500

    added authentication to keystone and added User schema

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -1,5 +1,11 @@
+import { createAuth } from '@keystone-next/auth'
 import { config, createSchema } from '@keystone-next/keystone/schema'
 import 'dotenv/config'
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session'
+import { User } from './schemas/User'
 
 const databaseURL =
   process.env.DATABASE_URL || 'mongodb://localhost/online-gallery-cluster'
@@ -9,23 +15,42 @@ const sessionConfig = {
   secret: process.env.COOKIE_SECRET,
 }
 
-export default config({
-  // @ts-ignore
-  server: {
-    cors: {
-      origin: [process.env.FRONTEND_URL],
-      credentials: true,
-    },
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+    // TODO: add in initial roles here
   },
-  db: {
-    adapter: 'mongoose',
-    url: databaseURL,
-    // TODO: add data seeding here
-  },
-  lists: createSchema({}),
-  ui: {
-    // TODO: change this for roles
-    isAccessAllowed: () => true,
-  },
-  // TODO: add session values here
 })
+
+export default withAuth(
+  config({
+    // @ts-ignore
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: 'mongoose',
+      url: databaseURL,
+      // TODO: add data seeding here
+    },
+    lists: createSchema({
+      User,
+    }),
+    ui: {
+      // Show the UI only for people who pass this text
+      isAccessAllowed: ({ session }) => {
+        console.log(session)
+        return !!session?.data
+      },
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+      User: 'id name email',
+    }),
+  }),
+)
