aeca71fe9ae4f76b78bfca4022d3f38e2c2198de
Lan Phan <lanc_phan@hotmail.com>
2021-12-16 00:16:05 -0500

    display the products in a grid

--- frontend/components/Products.js
+++ frontend/components/Products.js
@@ -1,5 +1,6 @@
 import { useQuery } from '@apollo/client';
 import gql from 'graphql-tag';
+import styled from 'styled-components';
 import Product from './Product';
 
 const ALL_PRODUCTS_QUERY = gql`
@@ -19,17 +20,23 @@ const ALL_PRODUCTS_QUERY = gql`
   }
 `;
 
+const ProductsListStyles = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 60px;
+`;
+
 export default function Products() {
   const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY);
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
   return (
     <div>
-      <div>
+      <ProductsListStyles>
         {data.allProducts.map((product) => (
           <Product key={product.id} product={product} />
         ))}
-      </div>
+      </ProductsListStyles>
     </div>
   );
 }
